28f525b3dbb488bfb61216ea25cc86ed3757c191
Laxynium <grzesiu15432@gmail.com>
2023-02-05 13:55:33 +0100

    Remove not needed test

--- AdminItems.Tests/CreateAdminItemEndpointTests.cs
+++ AdminItems.Tests/CreateAdminItemEndpointTests.cs
@@ -1,28 +1,10 @@
-﻿using System.Text;
-using System.Text.Json;
-using AdminItems.Api;
+﻿using AdminItems.Api;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace AdminItems.Tests;
 
 public class CreateAdminItemEndpointTests
 {
-    [Fact]
-    public async Task post_a_valid_admin_item()
-    {
-        var request = new
-        {
-            code = "GFJS1234",
-            name = "First Admin Item",
-            comments = "This is a first admin item in system"
-        };
-
-        var response = await PostAdminItem(request);
-
-        response.Should().BeSuccessful();
-    }
-
     [Theory]
     [InlineData("GFJS1234", "First Admin Item", "This is a first admin item in system")]
     [InlineData("YTRA1235", "Another admin Item", "")]
@@ -40,16 +22,5 @@ public class CreateAdminItemEndpointTests
             request.code,
             request.name,
             request.comments));
-    }
-
-    private async Task<HttpResponseMessage> PostAdminItem(object request)
-    {
-        var factory = new WebApplicationFactory<Program>();
-        var client = factory.CreateClient();
-
-        var serialized = JsonSerializer.Serialize(request);
-        var content = new StringContent(serialized, Encoding.UTF8, "application/json");
-        
-        return await client.PostAsync("adminItems", content);
     }
 }
