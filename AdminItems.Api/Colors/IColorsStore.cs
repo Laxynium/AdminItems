c06e7f912e47fa1eac25b2f5bf4a399bf8d40a61
Laxynium <grzesiu15432@gmail.com>
2023-02-05 23:56:20 +0100

    Fix issues after running application

--- AdminItems.Api/Colors/IColorsStore.cs
+++ AdminItems.Api/Colors/IColorsStore.cs
@@ -8,7 +8,7 @@ public interface IColorsStore
 
 internal sealed class NullColorsStore : IColorsStore
 {
-    public Task<Color?> Find(long colorId) => null!;
+    public Task<Color?> Find(long colorId) => Task.FromResult((Color?)null);
 
     public Task<IReadOnlyList<TResult>> GetAll<TResult>(Func<Color, TResult> mapper) => 
         Task.FromResult<IReadOnlyList<TResult>>(new List<TResult>());
--- AdminItems.Api/Program.cs
+++ AdminItems.Api/Program.cs
@@ -9,7 +9,10 @@ builder.Services.AddSingleton<IAdminItemIdGenerator, IdGenAdminItemIdGenerator>(
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.CustomSchemaIds(type => type.FullName);
+});
 
 var app = builder.Build();
 
