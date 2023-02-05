0f990af8a39ef5c3b86d7ec8d5712c42ce7bd455
Laxynium <grzesiu15432@gmail.com>
2023-02-05 15:21:12 +0100

    Remove repeated test

--- AdminItems.Tests/GetColorsEndpointTests.cs
+++ AdminItems.Tests/GetColorsEndpointTests.cs
@@ -1,5 +1,4 @@
-﻿using AdminItems.Api;
-using AdminItems.Api.Colors;
+﻿using AdminItems.Api.Colors;
 using FluentAssertions;
 
 namespace AdminItems.Tests;
@@ -15,36 +14,7 @@ public class GetColorsEndpointTests
 
         response.Should().Be200Ok();
     }
-
-    [Fact]
-    public async Task hardcoded_colors_are_returned()
-    {
-        var colorsStore = new FakeColorsStore();
-        colorsStore.AddColors(new []
-        {
-            new Color(1, "alizarin"),
-            new Color(2, "amethyst"),
-            new Color(3, "beige"),
-            new Color(4, "cherry"),
-        });
-        var apiFactory = new AdminItemsApi();
-        apiFactory.UseStore(colorsStore);
-        
-        var response = await apiFactory.GetColors();
-
-        response.Should().Be200Ok()
-            .And.BeAs(new
-            {
-                items = new []
-                {
-                    new {id = 1, name = "alizarin"},
-                    new {id = 2, name = "amethyst"},
-                    new {id = 3, name = "beige"},
-                    new {id = 4, name = "cherry"},
-                } 
-            });
-    }
-
+    
     [Fact]
     public async Task colors_defined_in_colors_store_are_returned()
     {
