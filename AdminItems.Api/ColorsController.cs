e149984e76734aeaf6e1f200a6e5ae2bcb43933c
Laxynium <grzesiu15432@gmail.com>
2023-02-05 15:01:27 +0100

    Four hard coded colors are returned

--- AdminItems.Api/ColorsController.cs
+++ AdminItems.Api/ColorsController.cs
@@ -2,14 +2,24 @@
 
 namespace AdminItems.Api;
 
+public record Response(List<ColorDto> Items);
+public record ColorDto(long Id, string Name);
+
 [ApiController]
 [Route("[controller]")]
 public class ColorsController : ControllerBase
 {
 
     [HttpGet]
-    public Task Get()
+    public Task<Response> Get()
     {
-        return Task.CompletedTask;
+        var response = new Response(new List<ColorDto>
+        {
+            new(1, "alizarin"),
+            new(2, "amethyst"),
+            new(3, "beige"),
+            new(4, "cherry"),
+        });
+        return Task.FromResult(response);
     }
 }
--- AdminItems.Tests/GetColorsEndpointTests.cs
+++ AdminItems.Tests/GetColorsEndpointTests.cs
@@ -13,4 +13,24 @@ public class GetColorsEndpointTests
 
         response.Should().Be200Ok();
     }
+
+    [Fact]
+    public async Task hardcoded_colors_are_returned()
+    {
+        var apiFactory = new AdminItemsApi();
+        
+        var response = await apiFactory.GetColors();
+
+        response.Should().Be200Ok()
+            .And.BeAs(new
+            {
+                items = new []
+                {
+                    new {id = 1, name = "alizarin"},
+                    new {id = 2, name = "amethyst"},
+                    new {id = 3, name = "beige"},
+                    new {id = 4, name = "cherry"},
+                } 
+            });
+    }
 }
