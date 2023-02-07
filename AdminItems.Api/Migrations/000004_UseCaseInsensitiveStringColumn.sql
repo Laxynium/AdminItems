143bcf558c98c72cf73eb684f5f1e5f87222b1e4
Grzegorz Gruszczyk <ggruszczyk@kpmg.pl>
2023-02-08 00:27:57 +0100

    Use citext to make sure values are cae insensitive unique

---
+++ AdminItems.Api/Migrations/000004_UseCaseInsensitiveStringColumn.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "admin_items" ALTER COLUMN "code" TYPE citext;
+ALTER TABLE "admin_items" ALTER COLUMN "name" TYPE citext;
--- AdminItems.Migrator/Migrator.cs
+++ AdminItems.Migrator/Migrator.cs
@@ -24,6 +24,7 @@ public class Migrator
                 .PostgresqlDatabase(connectionString)
                 .WithScriptsFromFileSystem(scriptsPath)
                 .LogToAutodetectedLog()
+                .WithExecutionTimeout(TimeSpan.FromSeconds(180))
                 .BuildConfiguration();
 
         return new Migrator(configuration, loggerFactory);
