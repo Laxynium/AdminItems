4d7cf93ad7ad6b0b65623bd220dde3927288a6c7
Grzegorz Gruszczyk <ggruszczyk@kpmg.pl>
2023-02-07 13:41:49 +0100

    Accept order by as parameter

--- AdminItems.Api/AdminItems/Features/GetAdminItems/GetAdminItemsController.cs
+++ AdminItems.Api/AdminItems/Features/GetAdminItems/GetAdminItemsController.cs
@@ -6,6 +6,7 @@ using SqlKata.Execution;
 
 namespace AdminItems.Api.AdminItems.Features.GetAdminItems;
 
+public record Request(string OrderBy = "code asc");
 public record Response(IReadOnlyList<AdminItemResponse> Items);
 
 public record AdminItemResponse(long Id, string Code, string Name, string Color);
@@ -22,14 +23,18 @@ public class GetAdminItemsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<Response>> Get()
+    public async Task<ActionResult<Response>> Get([FromQuery] Request request)
     {
-        var query = new Query("admin_items")
-            .Select("id", "code", "name", "color")
-            .OrderBy("name")
-            .Slice(column: "name");
+        var ordering = ParseOrderBy(request.OrderBy);
 
-        query = SortBy(query, "code");
+        if (ordering is not {ordering:{order:{} order, column:{} column}})
+            return ordering.error!;
+
+        var query = new Query("admin_items")
+            .Select("id", "code", "name", "color");
+
+        query = SortBy(query, column);
+        query = query.Slice(column: column);
         
         var postgresCompiler = new PostgresCompiler();
         var db = new QueryFactory(_connection, postgresCompiler);
@@ -43,8 +48,15 @@ public class GetAdminItemsController : ControllerBase
         return Ok(new Response(items));
     }
 
+    private static ((string column, string order)? ordering, BadRequestObjectResult? error) ParseOrderBy(string orderBy)
+    {
+        var split = orderBy.Split(" ");
+        var column = split[0];
+        var order = split[1];
+
+        return ((column, order), null);
+    }
     private record AdminItemRecord(long Id, string Code, string Name, string Color, long RowNumber);
-    
     private static Query SortBy(Query query, string fieldName)
     {
         switch (fieldName)
