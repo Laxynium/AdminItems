0cb9b24c7d2d263d52e405c10d409ed8df374e3e
Grzegorz Gruszczyk <ggruszczyk@kpmg.pl>
2023-02-07 11:10:46 +0100

    Reformat

--- AdminItems.IntegrationTests/GetAdminItemsTests.cs
+++ AdminItems.IntegrationTests/GetAdminItemsTests.cs
@@ -15,11 +15,11 @@ public class GetAdminItemsTests : IntegrationTest
     [InlineData("432DAD", "Another random one", "Some descriptive comment", "blue")]
     public async Task response_contains_created_admin_item(string code, string name, string? comments, string color)
     {
-        await Run<IAdminItemsStore>(store => store.Add(AdminItemId.Create(1),new AdminItem(
+        await Run<IAdminItemsStore>(store => store.Add(AdminItemId.Create(1), new AdminItem(
             code, name, comments ?? string.Empty, color)));
-        
+
         var response = await Api.GetAdminItems();
-        
+
         response.Should().Be200Ok()
             .And.BeAs(new
             {
@@ -36,52 +36,30 @@ public class GetAdminItemsTests : IntegrationTest
                 }
             });
     }
-    
+
     [Fact]
     public async Task by_default_admin_items_are_ordered_by_code()
     {
         await Run<IAdminItemsStore>(store => store.Add(AdminItemId.Create(1),
-            new AdminItem("BBB13", "Admin Item1", "Some comment 1","red")));
-        
+            new AdminItem("BBB13", "Admin Item1", "Some comment 1", "red")));
+
         await Run<IAdminItemsStore>(store => store.Add(AdminItemId.Create(2),
-            new AdminItem("BBB12", "Admin Item2", "Some comment 2","blue")));
-        
+            new AdminItem("BBB12", "Admin Item2", "Some comment 2", "blue")));
+
         await Run<IAdminItemsStore>(store => store.Add(AdminItemId.Create(3),
-            new AdminItem("AAA11", "Admin Item3", "Some comment 3","green")));
+            new AdminItem("AAA11", "Admin Item3", "Some comment 3", "green")));
 
         var response = await Api.GetAdminItems();
-        
+
         response.Should().Be200Ok()
             .And.BeAs(new
             {
                 items = new[]
                 {
-                    new
-                    {
-                        id = 3,
-                        code = "AAA11",
-                        name = "Admin Item3",
-                        color = "green",
-                        comments = "Some comment 3"
-                    },
-                    new
-                    {
-                        id = 2,
-                        code = "BBB12",
-                        name = "Admin Item2",
-                        color = "blue",
-                        comments = "Some comment 2"
-                    },
-                    new
-                    {
-                        id = 1,
-                        code = "BBB13",
-                        name = "Admin Item1",
-                        color = "red",
-                        comments = "Some comment 1"
-                    }
+                    new { id = 3, code = "AAA11", name = "Admin Item3", color = "green", comments = "Some comment 3" },
+                    new { id = 2, code = "BBB12", name = "Admin Item2", color = "blue", comments = "Some comment 2" },
+                    new { id = 1, code = "BBB13", name = "Admin Item1", color = "red", comments = "Some comment 1" }
                 }
             }, opt => opt.WithStrictOrderingFor(x => x.items));
     }
-
 }
