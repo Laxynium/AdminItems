83125ba2d0dc75f569b3be18e144c4bbce0bd135
Grzegorz Gruszczyk <ggruszczyk@kpmg.pl>
2023-02-07 13:27:44 +0100

    Use SqlKata to construct query

--- AdminItems.Api/AdminItems/Features/GetAdminItems/GetAdminItemsController.cs
+++ AdminItems.Api/AdminItems/Features/GetAdminItems/GetAdminItemsController.cs
@@ -1,12 +1,15 @@
-﻿using Dapper;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Npgsql;
+using SqlKata;
+using SqlKata.Compilers;
+using SqlKata.Execution;
 
 namespace AdminItems.Api.AdminItems.Features.GetAdminItems;
 
 public record Response(IReadOnlyList<AdminItemResponse> Items);
+
 public record AdminItemResponse(long Id, string Code, string Name, string Color);
-    
+
 [ApiController]
 [Route("adminItems")]
 public class GetAdminItemsController : ControllerBase
@@ -17,37 +20,40 @@ public class GetAdminItemsController : ControllerBase
     {
         _connection = connection;
     }
-    
+
     [HttpGet]
     public async Task<ActionResult<Response>> Get()
     {
-        var sqlBuilder = new SqlBuilder();
-        sqlBuilder = SortBy(sqlBuilder, "code");
+        var query = new Query("admin_items")
+            .Select("id", "code", "name", "color")
+            .OrderBy("name")
+            .Slice(column: "name");
+
+        query = SortBy(query, "code");
         
-        var template = GetTemplate(sqlBuilder);
-        var result = await _connection.QueryAsync<AdminItemResponse>(
-            template.RawSql, template.Parameters);
-        return Ok(new Response(result.ToList()));
+        var postgresCompiler = new PostgresCompiler();
+        var db = new QueryFactory(_connection, postgresCompiler);
+
+        var queryResult = await db.FromQuery(query).GetAsync<AdminItemRecord>();
+
+        var items = queryResult
+            .Select(x => new AdminItemResponse(x.Id, x.Code, x.Name, x.Color))
+            .ToList();
+        
+        return Ok(new Response(items));
     }
 
-    private static SqlBuilder.Template GetTemplate(SqlBuilder builder) =>
-        new(builder,@"
-SELECT ""id""
-    , ""code""
-    , ""name""
-    , ""color""
-FROM ""admin_items""
-/**orderby**/", new { });
-
-    private static SqlBuilder SortBy(SqlBuilder sqlBuilder, string fieldName)
+    private record AdminItemRecord(long Id, string Code, string Name, string Color, long RowNumber);
+    
+    private static Query SortBy(Query query, string fieldName)
     {
         switch (fieldName)
         {
             case "code":
-                sqlBuilder.OrderBy("code");
+                query.OrderBy("code");
                 break;
         }
 
-        return sqlBuilder;
+        return query;
     }
 }
---
+++ AdminItems.Api/AdminItems/Features/GetAdminItems/QueryExtensions.cs
@@ -0,0 +1,100 @@
+﻿using System.Diagnostics.Contracts;
+using SqlKata;
+using SqlKata.Compilers;
+
+namespace AdminItems.Api.AdminItems.Features.GetAdminItems;
+
+/// <summary>
+/// Cursor based pagination taken from article
+/// https://www.corstianboerman.com/blog/2019-03-06/cursor-based-pagination-with-sql-server
+/// </summary>
+internal static class QueryExtensions
+{
+    [Pure]
+    public static Query Slice(
+        this Query query,
+        string after = null,
+        int first = 0,
+        string before = null,
+        int last = 0,
+        string column = "Id")
+    {
+        var queryClone = query.Clone();
+
+        // Manually compile the order clauses for later use in the query
+        var order = new SqlServerCompiler()
+            .CompileOrders(new SqlResult
+            {
+                Query = queryClone
+            });
+
+        queryClone.Clauses.RemoveAll(q => q.Component == "order");
+
+        if (string.IsNullOrWhiteSpace(order))
+            throw new Exception($"{nameof(query)} does not have an order by clause");
+
+        queryClone.SelectRaw($"ROW_NUMBER() OVER({order}) AS [RowNumber]");
+
+        var internalQuery = new Query()
+            .With("q", queryClone)
+            .From("q");
+
+        // Select all rows after provided cursor
+        if (!String.IsNullOrWhiteSpace(after))
+        {
+            internalQuery.Where("RowNumber", ">",
+                new Query("q")
+                    .Select("RowNumber")
+                    .Where(column, after));
+        }
+
+        // Select all rows before provided cursor
+        if (!String.IsNullOrWhiteSpace(before))
+        {
+            internalQuery.Where("RowNumber", "<",
+                new Query("q")
+                    .Select("RowNumber")
+                    .Where(column, before));
+        }
+
+        // Select the first x amount of rows
+        if (first > 0)
+        {
+            // If the after cursor is defined
+            if (!String.IsNullOrWhiteSpace(after))
+            {
+                internalQuery.Where("RowNumber", "<=",
+                    new Query("q")
+                        .SelectRaw($"[RowNumber] + {first}")
+                        .Where(column, after));
+            }
+            // If no after cursor is defined
+            else
+            {
+                internalQuery.Where("RowNumber", "<=", first);
+            }
+        }
+
+        // Select the last x amount of rows
+        if (last > 0)
+        {
+            // If the before cursor is defined
+            if (!String.IsNullOrWhiteSpace(before))
+            {
+                internalQuery.Where("RowNumber", ">=",
+                    new Query("q")
+                        .SelectRaw($"[RowNumber] - {last}")
+                        .Where(column, before));
+            }
+            // If we have to take data all the way from the back
+            else
+            {
+                internalQuery.Where("RowNumber", ">",
+                    new Query("q")
+                        .SelectRaw($"MAX([RowNumber]) - {last}"));
+            }
+        }
+
+        return internalQuery;
+    }
+}
